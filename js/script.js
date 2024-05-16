0407d08453d5eef20508b5fd76d6a1e9ae20cb7f
fkauhe12 <fkauhe12@gmail.com>
2024-05-16 10:42:09 -0300

    Parte 3

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function clickMenu() {
+    let menu = document.getElementById("menu-itens");
+    if (menu.style.display === "block") {
+        menu.
+  
+style.display = "none";
+    } 
+else {
+        menu. 
+style.display = "block";
+    }
+}
